6c1436122f1bd91a837e30d97a4c2d0505328b6f
AlaaNayef <62764662+AlaaNayef@users.noreply.github.com>
2020-03-28 22:59:41 +0300

    matrix and stack tasks
    matrix and stack tasks

--- generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
+++ generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
@@ -1,23 +1,71 @@
 package jo.edu.htu.upskilling.utils;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Stack<ELEMENT> {
+    ArrayList<ELEMENT> stackList = new ArrayList<>();
+    int stackCapacity;
 
     public Stack(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("negative size");
+        }
+        stackList.ensureCapacity(capacity);
+        stackCapacity = capacity;
     }
 
     public int capacity() {
-        return -1;
+        return stackCapacity;
     }
 
     public int count() {
-        return -1;
+        return stackList.size();
     }
 
     public ELEMENT pop() {
-        return null;
+        if (stackList.size() == 0) {
+            throw new IllegalStateException("stack is empty");
+        }
+
+
+        ELEMENT peak = stackList.remove(stackList.size() - 1);
+        return peak;
+
     }
 
     public void push(ELEMENT element) {
+        if (stackCapacity == (stackList.size())) {
+            throw new IllegalStateException("stack is full");
+        }
+        stackList.add(element);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        Stack newStack = (Stack) o;
+        if (newStack == null) {
+            return false;
+        }
+
+        System.out.println( (this.stackList.size() == newStack.stackList.size()));
+
+        if (this.stackList.size() == newStack.stackList.size()) {
+            for (int i = 0; i < stackList.size(); i++) {
+                if (stackList.get(i) == newStack.stackList.get(i)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stackList);
     }
 }
--- matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
+++ matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
@@ -1,6 +1,8 @@
 package jo.edu.htu.utils;
 
 public class Matrix {
+    private int[][] myMatrix;
+
     public Matrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             if (matrix[row] == null)
@@ -8,22 +10,41 @@ public class Matrix {
             if (matrix[row].length != matrix[0].length)
                 throw new IllegalArgumentException("Inconsistent rows");
         }
+        this.myMatrix = new int[matrix.length][matrix[0].length];
+        setMyMatrix(matrix);
+
+    }
+
+    private void setMyMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                this.myMatrix[row][col] = matrix[row][col];
+            }
+        }
     }
 
     public int rows() {
-        return -1;
+        return this.myMatrix.length;
     }
 
     public int cols() {
-        return -1;
+        return this.myMatrix[0].length;
     }
 
     public int value(int row, int col) {
-        return 0;
+
+        if ((row < 0 || row >= this.myMatrix.length) || (col < 0 || col >= this.myMatrix[0].length)) {
+            throw new IllegalArgumentException("invalid cell index: " + row + "," + col);
+        }
+        return this.myMatrix[row][col];
     }
 
     public void forEach(ValueConsumer valueConsumer) {
-
+        for (int row = 0; row < myMatrix.length; row++) {
+            for (int col = 0; col < myMatrix[row].length; col++) {
+                valueConsumer.accept(row, col, myMatrix[row][col]);
+            }
+        }
     }
 
     public interface ValueConsumer {
