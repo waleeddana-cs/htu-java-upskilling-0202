0a2d784e5aca4ee14c6d0972b9976f8f62374c01
waleeddana2019 <waleed2010.dana@gmail.com>
2020-03-29 16:41:31 +0300

    complete interface-practice (calculator) assignment

--- interfaces-practice/src/main/java/jo/edu/htu/upskiling/IntegerOperationsFactory.java
+++ interfaces-practice/src/main/java/jo/edu/htu/upskiling/IntegerOperationsFactory.java
@@ -4,21 +4,43 @@ public class IntegerOperationsFactory implements OperationsFactory<Integer> {
     // TODO you should implement this class
     @Override
     public Operation<Integer> summation() {
-        return null;
+        return new Operation<Integer>() {
+            @Override
+            public Integer execute(Integer first, Integer second) {
+                return first + second;
+            }
+        };
     }
 
     @Override
     public Operation<Integer> subtraction() {
-        return null;
+        return new Subtraction();
     }
 
     @Override
     public Operation<Integer> multiplication() {
-        return null;
+        return (first, second) -> first * second;
+
     }
 
     @Override
     public Operation<Integer> division() {
-        return null;
+        return new Division();
+    }
+
+    private class Division implements Operation<Integer> {
+
+        @Override
+        public Integer execute(Integer first, Integer second) {
+            return first / second;
+        }
+    }
+}
+
+class Subtraction implements Operation<Integer> {
+
+    @Override
+    public Integer execute(Integer first, Integer second) {
+        return first - second;
     }
 }
