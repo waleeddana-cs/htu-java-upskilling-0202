f58094176f012bafa0849f3d605c0fe41da870bc
farahallaham <FarahAllaham@htu.edu.jo>
2020-03-31 17:45:20 +0300

    edit

--- interfaces-practice/src/main/java/jo/edu/htu/upskiling/IntegerOperationsFactory.java
+++ interfaces-practice/src/main/java/jo/edu/htu/upskiling/IntegerOperationsFactory.java
@@ -5,12 +5,7 @@ public class IntegerOperationsFactory implements OperationsFactory<Integer> {
     // TODO you should implement this class
     @Override
     public Operation<Integer> summation() {
-        return new Operation<Integer>() {
-            @Override
-            public Integer execute(Integer first, Integer second) {
-                return first + second;
-            }
-        };
+        return (first, second) -> first + second;
     }
 
     @Override
