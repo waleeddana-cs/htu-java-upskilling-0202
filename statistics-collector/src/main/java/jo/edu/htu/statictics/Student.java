43bfea79ad21ad345212b4ad2120bcb96be38732
AlaaNayef <62764662+AlaaNayef@users.noreply.github.com>
2020-04-01 16:12:47 +0300

    student test succeeded

--- statistics-collector/src/main/java/jo/edu/htu/statictics/Student.java
+++ statistics-collector/src/main/java/jo/edu/htu/statictics/Student.java
@@ -3,67 +3,94 @@ package jo.edu.htu.statictics;
 import java.time.Year;
 
 public class Student {
+    private final String studentId;
+    private final String studentName;
+    private final Year studentGraduationYear;
+    private final  String studentUniversity;
+    private final String studentMajor;
+    private final Gender studentGender;
+
+    private Student(Builder builder) {
+        this.studentId = builder.studentId;
+        this.studentName = builder.studentName;
+        this.studentGraduationYear = builder.studentGraduationYear;
+        this.studentUniversity = builder.studentUniversity;
+        this.studentMajor = builder.studentMajor;
+        this.studentGender = builder.studentGender;
+    }
 
     public static Builder builder() {
+
         return new Builder();
     }
 
-    private Student() {
-
-    }
-
     public String getId() {
-        return null;
+        return studentId;
     }
 
     public String getName() {
-        return null;
+        return studentName;
     }
 
     public Year getGraduationYear() {
-        return null;
+        return studentGraduationYear;
     }
 
     public String getUniversity() {
-        return null;
+        return studentUniversity;
     }
 
     public String getMajor() {
-        return null;
+        return studentMajor;
     }
 
     public Gender getGender() {
-        return null;
+        return studentGender;
     }
 
     public static class Builder {
+        String studentId;
+        String studentName;
+        Year studentGraduationYear;
+        String studentUniversity;
+        String studentMajor;
+        Gender studentGender;
+
         public Builder setId(String id) {
+            this.studentId = id;
             return this;
         }
 
         public Builder setName(String name) {
+            this.studentName = name;
             return this;
         }
 
         public Builder setGraduationYear(Year graduationYear) {
+            this.studentGraduationYear = graduationYear;
             return this;
         }
 
         public Builder setUniversity(String university) {
+            this.studentUniversity = university;
             return this;
         }
 
 
         public Builder setMajor(String major) {
+            this.studentMajor = major;
             return this;
         }
 
         public Builder setGender(Gender gender) {
+            this.studentGender = gender;
             return this;
         }
 
         public Student build() {
-            return null;
+
+
+            return new Student(this);
         }
     }
 }
