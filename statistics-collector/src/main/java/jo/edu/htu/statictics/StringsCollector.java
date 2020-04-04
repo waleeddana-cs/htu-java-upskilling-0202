74712971a63d4f4c39880f9113e64348b90137e7
waleeddana-cs <waleeddana.cs@gmail.com>
2020-04-04 10:05:41 +0300

    complete statistics-collector assignment

--- statistics-collector/src/main/java/jo/edu/htu/statictics/StringsCollector.java
+++ statistics-collector/src/main/java/jo/edu/htu/statictics/StringsCollector.java
@@ -1,9 +1,52 @@
 package jo.edu.htu.statictics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringsCollector implements StatisticsCollector<String> {
+    List<Statistic> list = new ArrayList<>(4);
+    String[] keys = {"upper case letters", "lower case letters", "spaces count", "non-word characters"};
+    int[] numbers = new int[4];
 
     @Override
     public Iterable<Statistic> collect(Iterable<String> cases) {
-        return null;
+        computeNumbers(cases);
+        for (int i = 0; i < keys.length; i++) {
+            int finalI = i;
+            list.add(new Statistic() {
+                @Override
+                public String name() {
+                    return keys[finalI];
+                }
+
+                @Override
+                public Integer matchedCases() {
+                    return numbers[finalI];
+                }
+
+                @Override
+                public int compareTo(Statistic statistic) {
+                    return 0;
+                }
+            });
+        }
+        return list;
     }
+
+    private void computeNumbers(Iterable<String> cases) {
+        for (String aCase : cases) {
+            for (int i = 0; i < aCase.length(); i++) {
+                if (Character.isUpperCase(aCase.charAt(i)))
+                    numbers[0]++;
+                else if (Character.isLowerCase(aCase.charAt(i)))
+                    numbers[1]++;
+                else if (aCase.charAt(i) == ' ')
+                    numbers[2]++;
+                else if (!Character.isDigit(aCase.charAt(i)))
+                    numbers[3]++;
+            }
+        }
+    }
+
+
 }
--- statistics-collector/src/main/java/jo/edu/htu/statictics/StudentsCollector.java
+++ statistics-collector/src/main/java/jo/edu/htu/statictics/StudentsCollector.java
@@ -1,8 +1,53 @@
 package jo.edu.htu.statictics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentsCollector implements StatisticsCollector<Student> {
+    List<Statistic> list = new ArrayList<>(6);
+    String[] keys = {"CIS major", "Computer Science major", "graduation year <= 2016", "graduation year > 2016", "Males", "Females"};
+    int[] numbers = new int[6];
+
     @Override
     public Iterable<Statistic> collect(Iterable<Student> cases) {
-        return null;
+        computeNumbers(cases);
+        for (int i = 0; i < keys.length; i++) {
+            int finalI = i;
+            list.add(new Statistic() {
+                @Override
+                public String name() {
+                    return keys[finalI];
+                }
+
+                @Override
+                public Integer matchedCases() {
+                    return numbers[finalI];
+                }
+
+                @Override
+                public int compareTo(Statistic statistic) {
+                    return 0;
+                }
+            });
+
+        }
+        return list;
+    }
+
+    private void computeNumbers(Iterable<Student> cases) {
+        for (Student aCase : cases) {
+            if (aCase.getMajor().equals("CIS"))
+                numbers[0]++;
+            else
+                numbers[1]++;
+            if (aCase.getGraduationYear().getValue() <= 2016)
+                numbers[2]++;
+            else
+                numbers[3]++;
+            if (aCase.getGender().equals(Gender.MALE))
+                numbers[4]++;
+            else
+                numbers[5]++;
+        }
     }
 }
