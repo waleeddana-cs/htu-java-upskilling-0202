0e7419a5a8c5223dffea40e98f0f78ac3d0a0485
waleeddana-cs <waleeddana.cs@gmail.com>
2020-04-04 11:12:04 +0300

    complete Character counter test

--- map-practice/src/main/java/jo/edu/htu/map/practice/CharactersCounter.java
+++ map-practice/src/main/java/jo/edu/htu/map/practice/CharactersCounter.java
@@ -1,9 +1,18 @@
 package jo.edu.htu.map.practice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CharactersCounter {
+
     public Map<Character, Integer> countCharacters(String sentence) {
-        return null;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < sentence.length(); i++) {
+            Character temp = Character.toLowerCase(sentence.charAt(i));
+            if (temp >= 'a' && temp <= 'z')
+                map.put(temp, map.get(temp) != null ? map.get(temp) + 1 : 1);
+        }
+        return map;
     }
 }
